97194bf68b64d8807675c7904081fc17db7ed786
Eli <elipup@gmail.com>
2015-06-15 12:06:52 -0400

    Split install function into prompt and headless install.

--- nginx.sh
+++ nginx.sh
@@ -17,7 +17,7 @@ nginx_menu() {
             do_exit
         elif [ $RET -eq 0 ]; then
             case "$NGM" in
-            "1") install_nginx ;;
+            "1") nginx_prompt ;;
             "2") remove_nginx ;;
             "3") break ;;
             *) msgbox "How did you get here?" && exit 0 ;;
@@ -27,29 +27,12 @@ nginx_menu() {
 
 }
 
-install_nginx() {
-    log_exec sudo apt-get -y install nginx
-    RET=$?
-    if [ $RET -eq 1 ]; then
-        msgbox "Nginx failed to install."
-        return $RET
-    fi
-    
-    if [ -z $DOMAIN ]; then
-        DOMAIN=$(whiptail --backtitle "$( window_title )" --inputbox "Domain Name" 8 60 3>&1 1>&2 2>&3)
-        RET=$?
-        if [ $RET -eq 1 ]; then
-            unset DOMAIN
-            return $RET
-        else
-            export DOMAIN
-        fi
-    fi
-    
+nginx_prompt() {
+
     if [ -z $NGINXHOSTNAME ]; then
         NGINXHOSTNAME=$(whiptail --backtitle "$( window_title )" --inputbox "Hostname" 8 60 3>&1 1>&2 2>&3)
         RET=$?
-        if [ $RET -eq 1 ]; then
+        if [ $RET -ne 0 ]; then
 	       unset DOMAIN
 		  unset NGINXHOSTNAME
             return $RET
@@ -58,11 +41,41 @@ install_nginx() {
         fi
     fi
     
+    if [ -z $DOMAIN ]; then
+        DOMAIN=$(whiptail --backtitle "$( window_title )" --inputbox "Domain Name" 8 60 3>&1 1>&2 2>&3)
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            unset DOMAIN
+            return $RET
+        else
+            export DOMAIN
+        fi
+    fi
+    
+    install_nginx $NGINXHOSTNAME $DOMAIN
+}
+
+# $1 is hostname for nginx
+# $2 is domain name
+install_nginx() {
+
+    log "Installing nginx"
+
+    export NGINXHOSTNAME=$1
+    export DOMAIN=$2
+
+    log_exec sudo apt-get -y install nginx
+    RET=$?
+    if [ $RET -eq 1 ]; then
+        msgbox "Nginx failed to install."
+        return $RET
+    fi
+    
     sudo rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
     sudo cp templates/nginx-site /etc/nginx/sites-available/$NGINXHOSTNAME
     sudo sed -i -e "s/DOMAINNAME/$DOMAIN/" -e "s/HOSTNAME/$NGINXHOSTNAME/" /etc/nginx/sites-available/$NGINXHOSTNAME
     RET=$?
-    if [ $RET -eq 1 ]; then
+    if [ $RET -ne 0 ]; then
         msgbox "Error configuring nginx.  Check site file in /etc/nginx/sites-available"
 	   return $RET
     fi
@@ -89,6 +102,7 @@ install_nginx() {
 }
 
 remove_nginx() {
+
     log_exec apt-get -y remove nginx
     RET=$?
     return $RET
