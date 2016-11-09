e3fe32bf1eaeeca0c8e1184a416188f580f85e8b
Perry Clark <pclark@xtuple.com>
2016-11-08 19:43:39 -0500

    fixed munininstall

--- misc/installmunin.sh
+++ misc/installmunin.sh
@@ -2,20 +2,32 @@
 
 sudo apt-get -y install fail2ban
 sudo apt-get -y install ntp ntpdate
-sudo apt-get -y install munin-node libdbd-pg-perl
-
+sudo apt-get -y install libio-all-lwp-perl
+sudo apt-get -y install libdbd-pg-perl
+sudo apt-get -y install munin-node
 sudo munin-node-configure --suggest
 
-sudo ln -s /usr/share/munin/plugins/postgres_autovacuum /etc/munin/plugins/postgres_autovacuum
-sudo ln -s /usr/share/munin/plugins/postgres_bgwriter /etc/munin/plugins/postgres_bgwriter
-sudo ln -s /usr/share/munin/plugins/postgres_cache_	/etc/munin/plugins/postgres_cache_ALL
-sudo ln -s /usr/share/munin/plugins/postgres_checkpoints /etc/munin/plugins/postgres_checkpoints
-sudo ln -s /usr/share/munin/plugins/postgres_connections_ /etc/munin/plugins/postgres_connections_ALL
-sudo ln -s /usr/share/munin/plugins/postgres_connections_db /etc/munin/plugins/postgres_connections_db
-sudo ln -s /usr/share/munin/plugins/postgres_locks_ /etc/munin/plugins/postgres_locks_ALL                    
-sudo ln -s /usr/share/munin/plugins/postgres_querylength_ /etc/munin/plugins/postgres_querylength_ALL              
-sudo ln -s /usr/share/munin/plugins/postgres_size_ /etc/munin/plugins/postgres_size_ALL
-sudo ln -s /usr/share/munin/plugins/postgres_transactions_ /etc/munin/plugins/postgres_transactions_ALL
-sudo ln -s /usr/share/munin/plugins/postgres_users /etc/munin/plugins/postgres_users        
-sudo ln -s /usr/share/munin/plugins/postgres_xlog /etc/munin/plugins/postgres_xlog         
+sudo mkdir -p /var/lib/munin/.ssh
+
+(echo 'command="/bin/false",no-agent-forwarding,no-pty,no-user-rc,no-X11-forwarding,permitopen="localhost:4949" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4mp44eypjrsZWYMMrJcDPZKRlHUCb2anuh+uQOaro8pztGo9YwVHMOs8nauQ8yyop/iKk7dwfPgnzDxipw4U24Foxwu6iZLx4RPoPpe68fL/GBf6CbE0l6jHz+Q3XZq/lHi41563fBQXsELQKBFydNSTET+XrEHYEl8s1Yobm5zK5ILMbNwDDYeKTyCDy/OZj90B/NUOh4KewgL3WXZXTX03ji2j/1ZLKBK1pa6THx+dsypXj6aVu1fG6CkBNX3QrWLDNaZLvVqkN2Vjep20ZbQEeT+7ZR+AlyQ2iYeGd057MMqHi2416DuJ6ZXhFlD928M012dxDW6tdK9oXkEKz munin@xtuple'
+ echo ' ') | sudo tee -a /var/lib/munin/.ssh/authorized_keys >/dev/null
+
+sudo chown -R munin:munin /var/lib/munin
+sudo chown -R munin:munin /var/lib/munin/.ssh/authorized_keys
+
+#if nginx installed, then do this.
+
+if type "nginx" > /dev/null; then
+
+   wget https://github.com/perusio/nginx-munin/archive/master.zip
+
+   unzip master.zip
+
+   sudo cp nginx-munin-master/nginx_connection_request nginx_memory /etc/munin/plugins/ -fv
+
+   sudo cp nginx-munin-master/nginx_memory /etc/munin/plugins/ -fv
+
+fi
+
+sudo service munin-node restart
 
