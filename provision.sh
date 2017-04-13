fc5ffe139061749e232fec78fdf8c90dfb948445
Eli <elipup@gmail.com>
2017-04-13 16:15:12 -0400

    Merge provisioning options into two main choices.

--- provision.sh
+++ provision.sh
@@ -3,53 +3,34 @@ provision_menu() {
 
     log "Opened provisioning menu"
 
-    ACTIONS=$(whiptail --separate-output --title "Select Components" --checklist --cancel-button "Cancel" \
-    "Please choose the actions you would like to take" 15 60 7 \
-    "installpg" "Install PostgreSQL $POSTVER" ON \
-    "provisioncluster" "Provision PostgreSQL Cluster" ON \
-    "initdb" "Add xTuple admin user and role" ON \
-    "demodb" "Load xTuple Database" OFF \
-    "qtclient" "xTuple ERP Client" OFF \
-    "webclient" "Load xTuple Web Client" OFF \
-    "nginx" "Nginx" OFF \
+    ACTION=$(whiptail --backtitle "$( window_title )" --menu "Select Action" --ok-button "Select" --cancel-button "Cancel" \
+    "Please choose the action you would like to take" 15 60 7 \
+    "1" "Install PostBooks Database Only" ON \
+    "2" "Install PostBooks Database With Web Components" OFF \
     3>&1 1>&2 2>&3)
 
     RET=$?
     if [ $RET = 0 ]; then
-        if [[ $ACTIONS == *"installpg"* ]] && [[ $ACTIONS != *"provisioncluster"* ]]; then
-            msgbox "You are about to install PostgreSQL but not provision to any clusters. \nYou will need to create a cluster manually before you can initialize \nit for xTuple. If you have chosen to initialize the database or install a demo \nthose actions will be skipped."
-            SKIP=1
-            ACTIONS=`sed "/initdb/d" <<< "$ACTIONS"`
-            log "Skipping initdb because provisioncluster was not chosen."
-            ACTIONS=`sed "/demodb/d" <<< "$ACTIONS"`
-            log "Skipping demodb because provisioncluster was not chosen."
         fi
-        for i in $ACTIONS; do   
-            case "$i" in
-            "installpg") log_choice install_postgresql $POSTVER
-                           log_choice drop_cluster $POSTVER main auto
-                           ;;
-            "provisioncluster") log_choice provision_cluster $POSTVER
-                                ;;
-            "initdb") log_choice prepare_database auto
-                      ;;
-            "nginx") configure_nginx
-                     ;;
-            "qt-client") msgbox "Qt Client not implemented yet"
-                         ;;
-            "demodb") log_choice download_demo manual $WORKDIR/tmp.backup
-                      ;;
-            "webclient") log_choice install_mwc_menu
-                      ;;
-             *) ;;
-            esac || main_menu
-        done
+        if [ $ACTION = "1" ]; then
+            log_choice install_postgresql $POSTVER
+		    log_choice drop_cluster $POSTVER main auto
+			log_choice provision_cluster $POSTVER
+			log_choice prepare_database auto
+			configure_nginx
+			log_choice download_demo manual $WORKDIR/tmp.backup
+		else
+            log_choice install_postgresql $POSTVER
+			log_choice drop_cluster $POSTVER main auto
+			log_choice provision_cluster $POSTVER
+			log_choice prepare_database auto
+			configure_nginx
+			log_choice download_demo manual $WORKDIR/tmp.backup
+			log_choice install_mwc_menu
+		fi
     fi
-    
-    if [ -n "$ACTIONS" ]; then
-        msgbox "The following actions were completed: \n$ACTIONS" 
-    elif [ -z "$ACTIONS" ]; then
-        msgbox "No actions were taken."
-    fi
+
+    msgbox "Install Complete"
+
     return 0;
 }
