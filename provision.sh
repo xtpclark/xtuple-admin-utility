0f05a4971931a2ba6059787881aecd63bbed4736
Eli <elipup@gmail.com>
2017-04-25 13:21:11 -0400

    New Quick install menu replacing old Provisioning Menu.

--- provision.sh
+++ provision.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
+
 provision_menu() {
 
     log "Opened provisioning menu"
 
-    ACTION=$(whiptail --backtitle "$( window_title )" --menu "Select Action" --ok-button "Select" --cancel-button "Cancel" \
-    "Please choose the action you would like to take" 15 60 7 \
-    "1" "Install PostBooks Database Only" ON \
-    "2" "Install PostBooks Database With Web Components" OFF \
+    ACTION=$(whiptail --backtitle "$( window_title )" --menu "Select Action" 0 0 7 --ok-button "Select" --cancel-button "Cancel" \
+    "1" "Install PostBooks Database Only" \
+    "2" "Install PostBooks Database With Web Components" \
     3>&1 1>&2 2>&3)
 
     RET=$?
-    if [ $RET = 0 ]; then
-        fi
-        if [ $ACTION = "1" ]; then
-            log_choice install_postgresql $POSTVER
-		    log_choice drop_cluster $POSTVER main auto
-			log_choice provision_cluster $POSTVER
-			configure_nginx
-			log_choice download_demo manual $WORKDIR/tmp.backup
-		else
-            log_choice install_postgresql $POSTVER
-			log_choice drop_cluster $POSTVER main auto
-			log_choice provision_cluster $POSTVER
-			configure_nginx
-			log_choice download_demo manual $WORKDIR/tmp.backup
-			log_choice install_mwc_menu
-		fi
+    if [ $RET -ne 0 ]; then
+        return 0
+    elif [ $ACTION = "1" ]; then
+        log_choice install_postgresql $POSTVER
+        log_choice drop_cluster $POSTVER main auto
+        log_choice provision_cluster $POSTVER
+        log_choice download_demo manual $WORKDIR/tmp.backup
+    else
+        log_choice install_postgresql $POSTVER
+        log_choice drop_cluster $POSTVER main auto
+        log_choice provision_cluster $POSTVER
+        configure_nginx
+        log_choice download_demo manual $WORKDIR/tmp.backup
+        log_choice install_mwc_menu
     fi
-
     msgbox "Install Complete"
 
-    return 0;
+    return 0
 }
